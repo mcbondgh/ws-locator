805d00560a5c6e3b77b0c667ac7cdaf600138cd3
start.vaadin.com <no-reply@vaadin.com>
2023-07-14 14:34:00 +0000

    Generated project

---
+++ src/main/java/app/finsuit/data/service/SamplePersonRepository.java
@@ -0,0 +1,12 @@
+package app.finsuit.data.service;
+
+import app.finsuit.data.entity.SamplePerson;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface SamplePersonRepository
+        extends
+            JpaRepository<SamplePerson, Long>,
+            JpaSpecificationExecutor<SamplePerson> {
+
+}
---
+++ src/main/java/app/finsuit/data/service/UserRepository.java
@@ -0,0 +1,10 @@
+package app.finsuit.data.service;
+
+import app.finsuit.data.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
+
+    User findByUsername(String username);
+}
