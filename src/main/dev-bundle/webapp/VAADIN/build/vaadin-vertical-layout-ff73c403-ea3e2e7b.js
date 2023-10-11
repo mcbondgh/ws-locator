8b587b79fff25f7b4ef87b2460628448e2434449
mcbondgh <mcbondprince@yahoo.com>
2023-10-11 05:32:49 -1200

    Project continuation

---
+++ src/main/dev-bundle/webapp/VAADIN/build/vaadin-vertical-layout-ff73c403-ea3e2e7b.js
@@ -0,0 +1,1 @@
+import{U as a}from"./indexhtml-d4ebb10e.js";const o={tagName:"vaadin-vertical-layout",displayName:"VerticalLayout",elements:[{selector:"vaadin-vertical-layout",displayName:"Layout",properties:[a.backgroundColor,a.borderColor,a.borderWidth,a.borderRadius,a.padding,a.gap]}]};export{o as default};
