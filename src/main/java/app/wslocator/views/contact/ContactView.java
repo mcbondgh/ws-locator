c143a8b0f2bd02017b3f93779de98c7af17abeae
mcbondgh <mcbondprince@yahoo.com>
2023-10-19 07:58:04 -1200

    ContactView updated

--- src/main/java/app/wslocator/views/contact/ContactView.java
+++ src/main/java/app/wslocator/views/contact/ContactView.java
@@ -1,5 +1,9 @@
 package app.wslocator.views.contact;
 
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
@@ -14,6 +18,33 @@ import app.wslocator.views.layouts.HeaderFooterLayouts;
 @PageTitle("Contact Us")
 public class ContactView extends VerticalLayout {
 
-    public ContactView() {}
+    public ContactView() {
+        addClassName("contact-page-view");
+        add(
+                heroDisplayArea(),
+                pageContent()
+        );
+    }
+
+
+    private Component heroDisplayArea() {
+        VerticalLayout layout = new VerticalLayout();
+        H1 heroText = new H1("Get In Touch With Us 😀");
+        Text contentText = new Text("Want to know more about our operations or our agenda? We'd love to chat.");
+
+        heroText.addClassName("hero-text");
+        contentText.addClassName("content-text");
+        layout.addClassName("header-layout-container");
+
+        layout.add(heroText, contentText);
+        return layout;
+    }
+
+    private Component pageContent() {
+        HorizontalLayout layout = new HorizontalLayout();
+
+        return layout;
+    }
+
     
 }
